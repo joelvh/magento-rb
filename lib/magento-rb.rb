d638d975ad1d6d42826a1044b87a79a020816eb8
Joel Van Horn <joeldvanhorn@gmail.com>
2012-06-08 04:34:42 -0400

    added config properties

--- lib/magento-rb.rb
+++ lib/magento-rb.rb
@@ -2,4 +2,31 @@ require "magento/soap_api_v2"
 require "magento/xml_api"
 
 module Magento
+  
+  def self.api_endpoint=(value)
+    @api_endpoint = value
+  end
+  
+  def self.api_username=(value)
+    @api_username = value
+  end
+  
+  def self.api_password=(value)
+    @api_password = value
+  end
+  
+  def self.debug=(value)
+    @debug = value
+  end
+  
+  debug = false
+  
+  def self.xml_api
+    @client ||= XmlApi.new @api_endpoint, @api_username, @api_password, :debug => @debug
+  end
+  
+  def self.soap_api
+    @client ||= SoapApiV2.new @api_endpoint, @api_username, @api_password
+  end
+  
 end
